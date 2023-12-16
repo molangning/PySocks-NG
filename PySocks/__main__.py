fef0c5a171ca563043ef3703930f17c0e4157c48
Mo Langning <133737702+molangning@users.noreply.github.com>
2023-12-16 17:54:52 +0800

    Written some placeholder text

---
+++ PySocks/__main__.py
@@ -0,0 +1,5 @@
+"""
+Main file, called when executed like "python3 PySocks"
+Although this is a socks5 client and there is no practical usage for this,
+I decided to keep this here for now for future uses.
+"""
